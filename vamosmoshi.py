dcda7f6a0269f7bc7fb015712ef23937a5013b3e
maxogod <maxi18wars@gmail.com>
2022-12-03 11:07:35 -0300

    VamosMoshi class

--- vamosmoshi.py
+++ vamosmoshi.py
@@ -1,27 +1,42 @@
 import sys
 from constantes import *
-ARCHIVO = sys.argv[1]
+from procesar_pajek import crear_grafo_desde_pajek
 
 
-def comandos():
-    ingreso = input().lower()
-    while ingreso is not CORTAR:
-        comando = ingreso.split()
-        if comando[0] == COMANDO1:
-            pass
-        elif comando[0] == COMANDO2:
-            pass
-        elif comando[0] == COMANDO3:
-            pass
-        elif comando[0] == COMANDO4:
-            pass
+class VamosMoshi:
 
+    def __init__(self, args):
+        self.grafo = crear_grafo_desde_pajek(args)
+
+    def ejecutar(self):
         ingreso = input().lower()
+        while ingreso is not CORTAR:
+            comando = ingreso.split()
+            if comando[0] == COMANDO1:
+                pass
+            elif comando[0] == COMANDO2:
+                pass
+            elif comando[0] == COMANDO3:
+                pass
+            elif comando[0] == COMANDO4:
+                pass
 
+            ingreso = input().lower()
 
-def camino_minimo():
-    pass
+    def camino_minimo(self):
+        pass
+
+    def itinerario_recomendaciones(self):
+        pass
+
+    def recorrido_circular(self):
+        pass
+
+    def arbol_tendido_minimo(self):
+        pass
 
 
 if __name__ == '__main__':
-    comandos()
+    archivo = sys.argv[1]
+    programa = VamosMoshi(archivo)
+    programa.ejecutar()
